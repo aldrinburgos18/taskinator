b554c317f25fffa52c1f8decd4e48075ed5caae9
aldrinburgos <burgos.aldrin@gmail.com>
2020-10-16 16:01:59 -0500

    finishing up on 'drag and drop'

--- assets/js/script.js
+++ assets/js/script.js
@@ -194,7 +194,9 @@ var dropZoneDragHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
         event.preventDefault();
+        taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
+    
 };
 
 var dropTaskHandler = function(event) {
@@ -216,10 +218,19 @@ var dropTaskHandler = function(event) {
     }
 
     dropZoneEl.appendChild(draggableElement);
+    dropZoneEl.removeAttribute("style");
 };
+
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if(taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+}
 
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangedHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
